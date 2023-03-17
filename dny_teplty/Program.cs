f080e75fcda8aa15fc13a9dbdc03327aa903d7ea
zikalop <vojtazika2@gmail.com>
2023-03-17 12:13:15 +0100

    update(znova-možnost)

--- dny_teplty/Program.cs
+++ dny_teplty/Program.cs
@@ -1,5 +1,5 @@
 ﻿using Spectre.Console;
-
+ZNOVA:
 var poleMesicu = AnsiConsole.Prompt(  //vytvoří tabulky s měsíci
     new SelectionPrompt<string>()
         .Title("Vyber měsíc kdy si pamatuješ teploty")
@@ -24,6 +24,7 @@ var dny = new List<string>();
 for (int i = 1; i <= pocetDnu; i++) dny.Add($"{i}. den");
 dny.Add("Statistiky teplot");
 dny.Add("KONEC");
+dny.Add("Chceš začit znova?");
 while (cyklus) //vytvori cyklus na zapis teplot k dnum
 {
     Console.Clear();
@@ -46,8 +47,24 @@ while (cyklus) //vytvori cyklus na zapis teplot k dnum
         Console.WriteLine($"Minimální teplota je {teploty.Min()}°C");
         Console.WriteLine($"Maximální teplota je {teploty.Max()}°C");   //vytvoři statistiky pro zadané teploty a ukončí cyklus
         Console.WriteLine($"Průměrná teplota je {teploty.Average()}°C");
-        cyklus = false;
-        break;
+        Console.WriteLine("chceš pokračovat?");
+        string z = Console.ReadLine();
+        if (z == "ano")
+        {
+            Console.Clear();
+            var y = AnsiConsole.Prompt( //vytvoří tabulku s dny
+            new SelectionPrompt<string>()
+                .Title("Mam otázku?")
+                .PageSize(10)
+                .MoreChoicesText("[grey]Šipkami vybírej dny[/]")
+                .AddChoices("Chceš to opakovat znovu?"));
+            if (y == "Chceš to opakovat znovu?")
+            {
+                goto ZNOVA;
+            }
+            cyklus = false;
+            break;
+        }
     }
     float teplota = AnsiConsole.Ask<float>("Zadej tepotu: "); //zeptá se na teplotu kterou chcete zadat
     int pozice = dny.IndexOf(cisalDnu); //vytvoří int pozice index pro list dny
